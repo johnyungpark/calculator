8b3bafc496b3e40c8891745d29a3d6fec7880669
John Park <jpark985@gmail.com>
2022-09-01 16:44:00 -0500

    added some features and button functionality

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
 const display = document.querySelector('.display');
-const zero = document.querySelector('button.zero');
-const one = document.querySelector('button.one');
-const two = document.querySelector('button.two');
-const three = document.querySelector('button.three');
-const four = document.querySelector('button.four');
-const five = document.querySelector('button.five');
-const six = document.querySelector('button.six');
-const seven = document.querySelector('button.seven');
-const eight = document.querySelector('button.eight');
-const nine = document.querySelector('button.nine');
-const plus = document.querySelector('button.plus');
-const subt = document.querySelector('button.subt');
-const mult = document.querySelector('button.mult');
-const divi = document.querySelector('button.divi');
-const clear = document.querySelector('button.clear');
+let buttons = Array.from(document.getElementsByClassName('button'));
 const equal = document.querySelector('button.equal');
 
-zero.addEventListener('click', () => { (display.textContent += 0) });
-one.addEventListener('click', () => { (display.textContent += 1) });
-two.addEventListener('click', () => { (display.textContent += 2) });
-three.addEventListener('click', () => { (display.textContent += 3) });
-four.addEventListener('click', () => { (display.textContent += 4) });
-five.addEventListener('click', () => { (display.textContent += 5) });
-six.addEventListener('click', () => { (display.textContent += 6) });
-seven.addEventListener('click', () => { (display.textContent += 7) });
-eight.addEventListener('click', () => { (display.textContent += 8) });
-nine.addEventListener('click', () => { (display.textContent += 9) });
-plus.addEventListener('click', () => { (display.textContent += "+") });
-subt.addEventListener('click', () => { (display.textContent += "-")});
-mult.addEventListener('click', () => { (display.textContent += "*")});
-divi.addEventListener('click', () => { (display.textContent += "/")});
+window.addEventListener('keydown', keyboardInput);
 
-const erase = document.createElement('br');
-clear.addEventListener('click', () => { (display.innerText = '') + (display.insertAdjacentHTML('beforeend', '<br>'))});
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            case 'Clear':
+                display.innerText = '';
+                break;
+            case 'DEL':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case '=':
+                equate();
+                break;
+            case '.':
+                if (display.textContent.includes('.')) {
+                    return;
+                } else display.innerText += ".";
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
 
-equal.addEventListener('click', function () {
+function keyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) {
+        display.innerText += e.key;
+    } else if (e.key === '.') {
+        if (display.textContent.includes('.')) {
+            return;
+        } else display.innerText += ".";
+    } else if (e.key === 'Backspace') {
+        display.innerText = display.innerText.slice(0, -1);
+    } else if (e.key === 'Escape') {
+        display.innerText = '';
+    } else if (e.key === '+') {
+        display.innerText += '+';
+    } else if (e.key === '-') {
+        display.innerText += '-';
+    } else if (e.key === '*') {
+        display.innerText += '*';
+    } else if (e.key === '/') {
+        display.innerText += '/';
+    } else if (e.key === 'Enter' || e.key === '=') equate ();
+};
+
+// equal.addEventListener('click', equate);
+
+function equate() {
     if (display.innerText.indexOf("+") != -1) {
         let final = display.innerText.split('+');
         let total = 0;
@@ -55,52 +72,13 @@ equal.addEventListener('click', function () {
         for (let i = 1; i < final.length; i++) {
             total *= final[i];
         }
-        display.innerText = total;
+        display.innerText = Math.round(total * 100)/100;
     } else if (display.innerText.indexOf("/") != -1) {
         let final = display.innerText.split('/');
         let total = final[0];
         for (let i = 1; i < final.length; i++) {
             total /= final[i];
         }
-        let final2 = (total === Infinity) ? (display.innerText = "Error!") : (display.innerText = total);
+        let final2 = (total === Infinity) ? (display.innerText = "Error!") : (display.innerText = Math.round(total * 100)/100);
     }
-});
-
-function subtract(array) {
-    let total = array[0];
-    for (let i = 1; i < array.length; i++) {
-        total -= array[i];
-    }
-    alert(total);
-}
-
-function multiply(array) {
-    let total = array[0];
-    for (let i = 1; i < array.length; i++) {
-        total *= array[i];
-    }
-    alert(total);
-}
-
-function divide(array) {
-    let total = array[0];
-    for (let i = 1; i < array.length; i++) {
-        total /= array[i];
-    }
-    alert(total);
-}
-
-function operate() {
-    let a = +prompt("enter first number", "");
-    let b = +prompt("enter second number", "");
-    let c = prompt("what operator?", "+, -, *, /");
-    if (c === "+") {
-        add([a,b]);
-    } else if (c === "-") {
-        subtract([a,b]);
-    } else if (c === "*") {
-        multiply([a,b]);
-    } else if (c === "/") {
-        divide([a,b]);
-    }
-}
+};
